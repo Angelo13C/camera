ad70d4e27560b2b5aab9e4ed5b67ae36546ff333
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-05-31 19:40:26 +0200

    ✨ Add the Configuration trait

--- firmware/crates/core/src/configuration/mod.rs
+++ firmware/crates/core/src/configuration/mod.rs
@@ -1,2 +1,13 @@
+use self::{customization::Customization, peripherals::Peripherals};
+
 pub mod customization;
 pub mod peripherals;
+
+pub trait Configuration
+{
+	type Peripherals: Peripherals;
+	type Customization: Customization;
+
+	fn peripherals(&mut self) -> Self::Peripherals;
+	fn customization(&mut self) -> Self::Customization;
+}
